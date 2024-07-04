2b2f6f150d20d9e053228f250a54f2db983e1fd6
Brad Moyetones <brad.moyetones@gmail.com>
2024-07-03 21:56:45 -0500

    String array in productSlug

--- api/getProductBySlug.tsx
+++ api/getProductBySlug.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export function useGetProductBySlug(slug: string | []) {
+export function useGetProductBySlug(slug: string | string[]) {
     const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products?filters[slug][$eq]=${slug}&populate=*`
     const [result, setResult] = useState(null)
     const [loading, setLoading] = useState(true)
